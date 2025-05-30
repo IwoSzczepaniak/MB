edf8c555b0256e4e46d7445049e29be0fb2e2a4c
milosz <miloszziemba.poczta@gmail.com>
2025-05-30 20:58:04 +0200

    add mvp after refactor

---
+++ backend/main.py
@@ -0,0 +1,175 @@
+from utils import *
+
+
+def parse_bpmn_file(bpmn_file_path: Union[str, None]) -> ET.ElementTree:
+    if bpmn_file_path is None:
+        bpmn_file_path = "input_diagram.bpmn"
+    tree = ET.parse(bpmn_file_path)
+    return tree
+
+
+def create_bpmn_from_dataframe(
+    dataframe: pd.DataFrame,
+    case_id_key: str = "Case ID",
+    activity_key: str = "Activity",
+    timestamp_key: str = "Timestamp",
+) -> pm4py.BPMN:
+    dataframe = pm4py.format_dataframe(
+        dataframe,
+        case_id=case_id_key,
+        activity_key=activity_key,
+        timestamp_key=timestamp_key,
+    )
+    event_log = pm4py.convert_to_event_log(dataframe)
+    bpmn_model = pm4py.discover_bpmn_inductive(event_log)
+    return bpmn_model
+
+
+def convert_log_to_dataframe(log_path: str) -> pd.DataFrame:
+    dataframe = pd.read_csv(log_path)
+    return dataframe
+
+
+def convert_log_to_bpmn(
+    log_path,
+    case_id_field_name,
+    activity_field_name,
+    timestamp_field_name,
+    path_to_save_bpmn="input_diagram.bpmn",
+) -> pd.DataFrame:
+    dataframe = convert_log_to_dataframe(log_path)
+    bpmn_model = create_bpmn_from_dataframe(
+        dataframe, case_id_field_name, activity_field_name, timestamp_field_name
+    )
+    pm4py.write_bpmn(bpmn_model, path_to_save_bpmn)
+    return dataframe
+
+
+def add_roles_to_bpmn(
+    root: ET.Element, task_to_role: Dict[str, str]
+) -> Dict[str, float]:
+
+    add_lane_set(root, "custom_laneSet")
+
+    unique_roles = set(task_to_role.values())
+    role_to_vertical_position = {}
+    for role in unique_roles:
+        lane = add_lane(root, role)
+        _, y, bpmn_lane_id = add_lane_di(root, lane)
+        role = find_element_by_id(root, bpmn_lane_id)
+        role_to_vertical_position[role.get("name")] = y
+
+    return role_to_vertical_position
+
+
+def repair_tasks(
+    root: ET.Element, role_to_vertical_position: Dict[str, float]
+) -> Dict[str, float]:
+    tasks = get_all_tasks(root)
+    task_to_vertical_position = {}
+    for task in tasks:
+        task_id = task.get("id")
+        task_name = task.get("name")
+
+        bpmn_shape = find_bpmn_shape_by_bpmn_element(root, task_id)
+        bounds = bpmn_shape.findall("omgdc:Bounds", NS)[0]
+        bpmn_shape.remove(bounds)
+        role = task_to_role[task_name]
+        create_bounds_element(
+            parent=bpmn_shape,
+            x=float(bounds.get("x")),
+            y=role_to_vertical_position[role] + LANE_HEIGHT // 2 - 18,
+            width=float(bounds.get("width")),
+            height=float(bounds.get("height")),
+        )
+        task_to_vertical_position[task] = (
+            role_to_vertical_position[role] + LANE_HEIGHT // 2
+        )
+
+    return task_to_vertical_position
+
+
+def repair_waypoints(
+    root: ET.Element, task_to_vertical_position: Dict[ET.Element, float]
+) -> None:
+    tasks = get_all_tasks(root)
+
+    for task in tasks:
+        incoming = find_all_incoming_waypoints_for_task(task)
+        outgoing = find_all_outgoing_waypoints_for_task(task)
+
+        # x positions of waypoints are right, just their y need to be adjusted to their tasks
+        proper_y = task_to_vertical_position[task]
+
+        # for incoming arrows, only the y last pair of coordinates needs to be changed,
+        for sequence_id in incoming:
+            bpmn_edge = find_bpmn_edge_by_bpmn_element(tree.getroot(), sequence_id)
+            positions = []
+            elements = []
+            for element in bpmn_edge.findall("ns6:waypoint", NS):
+                elements.append(element)
+                positions.append((float(element.get("x")), float(element.get("y"))))
+
+            # removing all existing coordinates of waypoints
+            for element in elements:
+                bpmn_edge.remove(element)
+
+            last_x, last_y = positions.pop(-1)
+            positions.append((last_x, proper_y))
+            for x, y in positions:
+                ET.SubElement(
+                    bpmn_edge,
+                    f"{{{NS['ns6']}}}waypoint",
+                    {
+                        "x": str(x + 8),
+                        "y": str(y),
+                    },
+                )
+
+        # similar logic for outgoing, however here first y in first pair of coordinates needs to be adjusted
+        for sequence_id in outgoing:
+            bpmn_edge: ET.Element = find_bpmn_edge_by_bpmn_element(
+                tree.getroot(), sequence_id
+            )
+            positions = []
+            elements = []
+            for element in bpmn_edge.findall("ns6:waypoint", NS):
+                elements.append(element)
+                positions.append((float(element.get("x")), float(element.get("y"))))
+
+            for element in elements:
+                bpmn_edge.remove(element)
+
+            last_x, last_y = positions.pop(0)
+            positions.insert(0, (last_x, proper_y))
+            for x, y in positions:
+                ET.SubElement(
+                    bpmn_edge,
+                    f"{{{NS['ns6']}}}waypoint",
+                    {
+                        "x": str(x),
+                        "y": str(y),
+                    },
+                )
+
+
+if __name__ == "__main__":
+
+    bpmn_file_path = "input_diagram.bpmn"
+
+    dataframe = convert_log_to_bpmn(
+        log_path="example_logs/repairExample.csv",
+        case_id_field_name="Case ID",
+        activity_field_name="Activity",
+        timestamp_field_name="Start Timestamp",
+        path_to_save_bpmn=bpmn_file_path,
+    )
+
+    tree = parse_bpmn_file(bpmn_file_path)
+
+    task_to_role = get_task_role_map(dataframe, role_field_name="Resource")
+    role_to_vertical_position = add_roles_to_bpmn(tree.getroot(), task_to_role)
+    task_to_vertical_position = repair_tasks(tree.getroot(), role_to_vertical_position)
+    repair_waypoints(tree.getroot(), task_to_vertical_position)
+
+    tree.write("output_diagram.bpmn", encoding="utf-8", xml_declaration=True)
--- backend/utils.py
+++ backend/utils.py
@@ -1,26 +1,9 @@
+import pm4py
 import xml.etree.ElementTree as ET
 import pandas as pd
-import pm4py
-import os
-from typing import Tuple, List, Union, Dict, Set
 
-# Define namespaces for easier access
-NS = {
-    "bpmn": "http://www.omg.org/spec/BPMN/20100524/MODEL",
-    "bpmndi": "http://www.omg.org/spec/BPMN/20100524/DI",
-    "omgdc": "http://www.omg.org/spec/DD/20100524/DC",
-    "ns6": "http://www.omg.org/spec/DD/20100524/DI",
-}
-
-LANE_HEIGHT = 200
-INITIAL_LANE_HORIZONTAL_SHIFT_LEFT = 100
-INITIAL_LANE_HORIZONTAL_SHIFT_RIGHT = 250
-INITIAL_LANE_VERTICAL_SHIFT = 20
-
-
-def parse_bpmn_file(bpmn_file_path: str) -> ET.ElementTree:
-    tree = ET.parse(bpmn_file_path)
-    return tree
+from typing import Tuple, List, Union, Dict
+from constants import *
 
 
 def get_most_top_left_position(root: ET.Element) -> Tuple[float, float]:
@@ -206,31 +189,14 @@ def get_task_role_map(
     dataframe: pd.DataFrame,
     task_field_name: str = "Activity",
     role_field_name: str = "Role",
-) -> Tuple[Dict[str, str], Set[str]]:
+) -> Dict[str, str]:
     task_to_role = {}
     for _, data in dataframe.iterrows():
         # space is not allowed within BPMN or xml fields, that is why spaces are replaced with underscores
         role_name = data[role_field_name].replace(" ", "_")
         task_to_role[data[task_field_name]] = role_name
 
-    return task_to_role, set(task_to_role.values())
-
-
-def create_bpmn_from_dataframe(
-    dataframe: pd.DataFrame,
-    case_id_key: str = "Case ID",
-    activity_key: str = "Activity",
-    timestamp_key: str = "Timestamp",
-) -> pm4py.BPMN:
-    dataframe = pm4py.format_dataframe(
-        dataframe,
-        case_id=case_id_key,
-        activity_key=activity_key,
-        timestamp_key=timestamp_key,
-    )
-    event_log = pm4py.convert_to_event_log(dataframe)
-    bpmn_model = pm4py.discover_bpmn_inductive(event_log)
-    return bpmn_model
+    return task_to_role
 
 
 def create_bounds_element(
@@ -246,124 +212,3 @@ def create_bounds_element(
             "height": str(height),
         },
     )
-
-
-def convert_log_to_dataframe(log_path: str) -> pd.DataFrame:
-    dataframe = pd.read_csv(log_path)
-    return dataframe
-
-
-def convert_log_to_bpmn(
-    log_path,
-    case_id_field_name,
-    activity_field_name,
-    timestamp_field_name,
-    path_to_save_bpmn,
-) -> pd.DataFrame:
-    dataframe = convert_log_to_dataframe(log_path)
-    bpmn_model = create_bpmn_from_dataframe(
-        dataframe, case_id_field_name, activity_field_name, timestamp_field_name
-    )
-    pm4py.write_bpmn(bpmn_model, path_to_save_bpmn)
-    return dataframe
-
-
-def add_roles_to_bpmn(dataframe: pd.DataFrame):
-    pass
-
-
-if __name__ == "__main__":
-
-    dataframe = convert_log_to_bpmn(
-        "repairExample.csv",
-        "Case ID",
-        "Activity",
-        "Start Timestamp",
-        "input_diagram.bpmn",
-    )
-
-    tree = parse_bpmn_file("input_diagram.bpmn")
-
-    add_roles_to_bpmn(dataframe)
-    task_to_role, unqiue_roles = get_task_role_map(
-        dataframe, role_field_name="Resource"
-    )
-    lane_set = add_lane_set(tree.getroot(), "Custom_laneset")
-    role_position_map = {}
-    for role in unqiue_roles:
-        lane = add_lane(tree.getroot(), role)
-        x, y, bpmn_lane_id = add_lane_di(tree.getroot(), lane)
-        role = find_element_by_id(tree.getroot(), bpmn_lane_id)
-        role_position_map[role.get("name")] = (x, y)
-
-    tasks = get_all_tasks(tree.getroot())
-    task_to_y_position = {}
-    for task in tasks:
-        task_id = task.get("id")
-        task_name = task.get("name")
-        bpmn_shape = find_bpmn_shape_by_bpmn_element(tree.getroot(), task_id)
-        bounds = bpmn_shape.findall("omgdc:Bounds", NS)
-        role = task_to_role[task_name]
-        _, y = role_position_map[role]
-        result = bounds[0]
-        x = float(result.get("x"))
-        width = float(result.get("width"))
-        height = float(result.get("height"))
-        bpmn_shape.remove(result)
-        task_to_y_position[task] = y + LANE_HEIGHT // 2 - 18
-        create_bounds_element(bpmn_shape, x, y + LANE_HEIGHT // 2 - 18, width, height)
-
-    for task in tasks:
-        incoming = find_all_incoming_waypoints_for_task(task)
-        outgoing = find_all_outgoing_waypoints_for_task(task)
-
-        y_position = task_to_y_position[task]
-
-        for id in incoming:
-            bpmn_edge = find_bpmn_edge_by_bpmn_element(tree.getroot(), id)
-            positions = []
-            elements = []
-            for element in bpmn_edge.findall("ns6:waypoint", NS):
-                print(element.get("x"))
-                elements.append(element)
-                positions.append((float(element.get("x")), float(element.get("y"))))
-
-            for element in elements:
-                bpmn_edge.remove(element)
-
-            last_x, last_y = positions.pop(-1)
-            positions.append((last_x, y_position))
-            for x, y in positions:
-                ET.SubElement(
-                    bpmn_edge,
-                    f"{{{NS['ns6']}}}waypoint",
-                    {
-                        "x": str(x),
-                        "y": str(y),
-                    },
-                )
-
-        for id in outgoing:
-            bpmn_edge: ET.Element = find_bpmn_edge_by_bpmn_element(tree.getroot(), id)
-            positions = []
-            elements = []
-            for element in bpmn_edge.findall("ns6:waypoint", NS):
-                elements.append(element)
-                positions.append((float(element.get("x")), float(element.get("y"))))
-
-            for element in elements:
-                bpmn_edge.remove(element)
-
-            last_x, last_y = positions.pop(0)
-            positions.insert(0, (last_x, y_position))
-            for x, y in positions:
-                ET.SubElement(
-                    bpmn_edge,
-                    f"{{{NS['ns6']}}}waypoint",
-                    {
-                        "x": str(x),
-                        "y": str(y),
-                    },
-                )
-
-    tree.write("output_diagram.xml", encoding="utf-8", xml_declaration=True)
